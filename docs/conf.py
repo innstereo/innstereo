bccd78b181dcbb06068152730ab9a483b9200624
spiessbuerger <tobias47n9e@gmail.com>
2015-02-11 19:09:48 +0100

    removed non-ASCII character from conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -47,7 +47,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = 'innsbruck_stereographic'
-copyright = '2015, Tobias Schönberg'
+copyright = '2015, Tobias Schoenberg'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
