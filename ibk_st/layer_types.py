3348383916dbe1579edaf248f18ac4e062f4152b
spiessbuerger <tobias47n9e@gmail.com>
2015-02-24 20:18:03 +0100

    code cleanup

--- ibk_st/layer_types.py
+++ ibk_st/layer_types.py
@@ -1,9 +1,38 @@
 #!/usr/bin/python3
 
+"""
+This module contains the different layers or datasets the program uses.
+
+This module contains the PlaneLayer-class. The other classes inherit most
+settings and methods from the PlaneLayer-class. The other classes are
+FaulPlaneLayer, LineLayer and SmallCircleLayer. Instances of these classes
+are created in the MainWindow-class. During plot-redraws the current styling
+of each layer is queried from these classes. The settings are also called when
+the layer properties dialog is opened. Changes in the layer properties dialog
+are stored in these classes.
+"""
+
 from gi.repository import Gdk, GdkPixbuf
 
+
 class PlaneLayer(object):
+
+    """
+    The PlaneLayer class initializes many default settings and methods.
+
+    The settings are currently hardcoded. The methods conists of get-methods,
+    to retrieve settings and set-methods to update settings. Colors have an
+    additional get-rgba-method.
+    """
+
     def __init__(self, treestore, treeview):
+        """
+        Initalizes the PlaneLayer class with default settings.
+
+        The settings that are applied here are also set for the layers, that
+        inherit from the PlaneLayer class, even if some settings are not used
+        for certain layer-types.
+        """
         self.data_treestore = treestore
         self.data_treeview = treeview
         self.type = "plane"
@@ -63,10 +92,10 @@ class PlaneLayer(object):
         """
         line_color_alpha = int("0x{0}ff".format(
             self.line_color[1:]), base=16)
-        self.pixbuf_color = GdkPixbuf.Pixbuf.new(
+        pixbuf_color = GdkPixbuf.Pixbuf.new(
             GdkPixbuf.Colorspace.RGB, True, 8, 16, 16)
-        self.pixbuf_color.fill(line_color_alpha)
-        return self.pixbuf_color
+        pixbuf_color.fill(line_color_alpha)
+        return pixbuf_color
 
     def get_rgba(self):
         """
@@ -601,38 +630,83 @@ class PlaneLayer(object):
         self.contour_use_line_color = new_state
 
 class FaultPlaneLayer(PlaneLayer):
+
+    """
+    The FaulPlaneLayer-class overrides the type and label settings.
+
+    This class sets the type to "faultplane" and the label to "Faulplane layer".
+    All other settings and methods are inherited from the PlaneLayer-class.
+    """
+
     def __init__(self, treestore, treeview):
+        """
+        Initializes the FaulPlaneLayer-class. Sets the type and label.
+
+        The type is set to "faulplane" and the label is set to "Faulplane
+        layer". Requires a treestore and treeview to initialize.
+        """
         PlaneLayer.__init__(self, treestore, treeview)
         self.type = "faultplane"
         self.label = "Faultplane layer"
 
 class LineLayer(PlaneLayer):
+
     """
-    Inherits some functions and assignments from PlaneLayer, but doesn't use
-    all of them. Seems a good way of keeping the class tree flat.
+    The LineLayer class inherits from PlaneLayer but changes the type and label
+
+    The LineLayer class only changes the layer-type to "line" and the label to
+    "Linear layer". For initialization it needs a treestore and treeview which
+    are passed to the PlaneLayer class. The class also overrides the get_pixbuf
+    method, because the colored square from the layer-view should reflect the
+    fill of the marker.   
     """
+
     def __init__(self, treestore, treeview):
+        """
+        Initializes the LineLayer class with type = "line" and a label.
+
+        The initialization requires a treestore and treeview that hold the
+        data of the layer. They are passed to the PlaneLayer class. The
+        layer-type is set to line and the label to "Linear layer".
+        """
         PlaneLayer.__init__(self, treestore, treeview)
         self.type = "line"
         self.label = "Linear layer"
 
     def get_pixbuf(self):
         """
-        Takes a color string (format: "#aabbcc") and assigns it to a
-        Pixbuf. Pushes the pixbuf.
-        Returns a pixbuf that can be assigned to a layer.
+        This returns the pixbuf-color to be used for the squares in layer-view.
+
+        This method overrides the method in the PlaneLayer class. It returns
+        the color of the marker-fill instead of the line-color. This ensures,
+        that the colored squares reflect the color of the symbols in the
+        stereonet.
         """
         marker_color_alpha = int("0x{0}ff".format(
             self.marker_fill[1:]), base=16)
-        self.pixbuf_color = GdkPixbuf.Pixbuf.new(
+        pixbuf_color = GdkPixbuf.Pixbuf.new(
             GdkPixbuf.Colorspace.RGB, True, 8, 16, 16)
-        self.pixbuf_color.fill(marker_color_alpha)
-        return self.pixbuf_color
+        pixbuf_color.fill(marker_color_alpha)
+        return pixbuf_color
 
 class SmallCircleLayer(PlaneLayer):
+
+    """
+    The SmallCircleLayer-class is used for small-circle datasets.
+
+    It sets the type to "smallcircle" and the default label to "Small circle
+    layer". All other methods and settings are inherited from the PlaneLayer-
+    class.
+    """
+
     def __init__(self, treestore, treeview):
+        """
+        Initializes the SmallCircleLayer-class.
+
+        Expects a TreeStore and TreeView that are passed to the PlaneLayer-
+        class. The layer type is set to "smallcircle" and the label is set
+        to "Small cirlce layer".
+        """
         PlaneLayer.__init__(self, treestore, treeview)
         self.type = "smallcircle"
         self.label = "Small circle layer"
-
-
