33cabfec63aaf32ec7e44e7ede1868f659b64649
tobias47n9e <tobias47n9e@gmail.com>
2015-05-15 11:19:16 +0200

    docs: update to all conf.py

--- docs/de/conf.py
+++ docs/de/conf.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 #
-# innsbruck_stereographic documentation build configuration file, created by
+# innstereo documentation build configuration file, created by
 # sphinx-quickstart on Sun Feb  8 22:32:13 2015.
 #
 # This file is execfile()d with the current directory set to its
@@ -179,7 +179,7 @@ html_static_path = ['_static']
 #html_file_suffix = None
 
 # Output file base name for HTML help builder.
-htmlhelp_basename = 'innsbruck_stereographicdoc'
+htmlhelp_basename = 'innstereo_doc'
 
 
 # -- Options for LaTeX output ---------------------------------------------
@@ -199,7 +199,7 @@ latex_elements = {
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
-  ('index', 'innsbruck_stereographic.tex', 'innsbruck\\_stereographic Documentation',
+  ('index', 'innstereo.tex', 'innstereo Documentation',
    'Tobias Schoenberg', 'manual'),
 ]
 
@@ -229,7 +229,7 @@ latex_documents = [
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [
-    ('index', 'innsbruck_stereographic', 'innsbruck_stereographic Documentation',
+    ('index', 'innstereo', 'innstereo Documentation',
      ['Tobias Schoenberg'], 1)
 ]
 
@@ -243,8 +243,8 @@ man_pages = [
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
 texinfo_documents = [
-  ('index', 'innsbruck_stereographic', 'innsbruck_stereographic Documentation',
-   'Tobias Schoenberg', 'innsbruck_stereographic', 'One line description of project.',
+  ('index', 'innstereo', 'innstereo Documentation',
+   'Tobias Schoenberg', 'innstereo', 'One line description of project.',
    'Miscellaneous'),
 ]
 
