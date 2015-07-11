9de2066ac0e20287992bc63d5b8cb50864d56797
xquiet <matteo.pasotti@gmail.com>
2015-07-12 00:34:42 +0200

    updated header

--- innstereo/i18n.py
+++ innstereo/i18n.py
@@ -1,24 +1,23 @@
 #==============================================================================#
-#                     OpenStereoNet (fork of OpenStereo)                       #
-#                  Open-source, Multiplatform Stereonet Analysis               #
+#                                   InnStereo                                  #
 #                                                                              #
 #    Copyright (c)  2012-2015 Matteo Pasotti <matteo.pasotti@gmail.com>        #
 #                                                                              #
 #                                                                              #
-#    This file was originally part of OpenStereoNet.                           #
+#    This file was originally part of OpenStereoNet (fork of OpenStereo).      #
 #    This file is part of InnStereo since July 2015                            #
-#    InnStereo is free software: you can redistribute it and/or modify        #
+#    InnStereo is free software: you can redistribute it and/or modify         #
 #    it under the terms of the GNU General Public License as published by      #
 #    the Free Software Foundation, either version 2 of the License, or         #
 #    (at your option) any later version.                                       #
 #                                                                              #
-#    InnStereo is distributed in the hope that it will be useful,             #
+#    InnStereo is distributed in the hope that it will be useful,              #
 #    but WITHOUT ANY WARRANTY; without even the implied warranty of            #
 #    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the             #
 #    GNU General Public License for more details.                              #
 #                                                                              #
 #    You should have received a copy of the GNU General Public License         #
-#    along with InnStereo.  If not, see <http://www.gnu.org/licenses/>.       #
+#    along with InnStereo.  If not, see <http://www.gnu.org/licenses/>.        #
 #==============================================================================#
 # -*- coding: utf-8 -*-
 
@@ -77,3 +76,4 @@ class i18n:
 
   def get_ts_domain(self):
     return self.app_name
+  
